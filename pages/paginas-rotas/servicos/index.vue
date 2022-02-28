cbee9a99204260e47fd6a3c19e7ef00d5c2a1e34
willmarcel <marcell.luh@gmail.com>
2022-02-28 11:05:07 -0300

    paginas e rotas aula 03 - exemplo de templates

--- pages/paginas-rotas/servicos/index.vue
+++ pages/paginas-rotas/servicos/index.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
 
-    <div class="gb-gray-100">
-      <div class="container mx-auto flex space-x-4 h-100 items-center mb-10">
-        <NuxtLink to="/paginas-rotas/a-empresa">A Empresa</NuxtLink>
-        <NuxtLink to="/paginas-rotas/servicos">Serviços</NuxtLink>
-        <NuxtLink to="/paginas-rotas/produtos">Produtos</NuxtLink>
-      </div>
-    </div>
-
-    <div>
-      <div class="container mx-auto">
-        <h1 class="tetx-lg font-bold">Serviços</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta doloribus eos et eum eveniet harum ipsa porro quia rem veritatis! Ad beatae commodi cupiditate dignissimos iste magni perferendis quasi, repudiandae.</p>
-      </div>
-    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta doloribus eos et eum eveniet harum ipsa porro quia rem veritatis! Ad beatae commodi cupiditate dignissimos iste magni perferendis quasi, repudiandae.</p>
 
   </div>
 </template>
