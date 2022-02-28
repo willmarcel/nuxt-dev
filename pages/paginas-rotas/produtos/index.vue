67ed47d951f0cabbdc240cd4f89b6174684a80fc
willmarcel <marcell.luh@gmail.com>
2022-02-28 10:58:24 -0300

    paginas e rotas aula 02

--- pages/paginas-rotas/produtos/index.vue
+++ pages/paginas-rotas/produtos/index.vue
@@ -5,12 +5,13 @@
       <div class="container mx-auto flex space-x-4 h-100 items-center mb-10">
         <NuxtLink to="/paginas-rotas/a-empresa">A Empresa</NuxtLink>
         <NuxtLink to="/paginas-rotas/servicos">Serviços</NuxtLink>
+        <NuxtLink to="/paginas-rotas/produtos">Produtos</NuxtLink>
       </div>
     </div>
 
     <div>
       <div class="container mx-auto">
-        <h1 class="tetx-lg font-bold">A Empresa</h1>
+        <h1 class="tetx-lg font-bold">Produtos</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta doloribus eos et eum eveniet harum ipsa porro quia rem veritatis! Ad beatae commodi cupiditate dignissimos iste magni perferendis quasi, repudiandae.</p>
       </div>
     </div>
